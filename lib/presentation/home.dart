23ffc2364258025556497927c095d09abd100508
zahraaliloutfi <za5052557@gmail.com>
2023-09-15 18:36:11 +0300

    زهره

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -15,16 +15,29 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('counter value is'),
-              SizedBox(
+              const Text(
+                'counter value is',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
                 height: 20,
               ),
               BlocBuilder<CounterAppBloc, CounterAppState>(
                 builder: (context, state) {
                   if (state is CounterAppInitial) {
-                    return Text('0');
+                    return const Text(
+                      '0',
+                      style: TextStyle(fontSize: 40, color: Colors.blueGrey),
+                    );
                   } else if (state is CounterValueChangedState) {
-                    return Text(state.counter.toString());
+                    return Text(
+                      state.counter.toString(),
+                      style:
+                          const TextStyle(fontSize: 40, color: Colors.blueGrey),
+                    );
                   } else {
                     return Container();
                   }
@@ -37,7 +50,9 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                BlocProvider.of<CounterAppBloc>(context).add(IncrementEvent());
+              },
               child: const Icon(
                 Icons.add,
               ),
@@ -46,7 +61,11 @@ class HomeScreen extends StatelessWidget {
               height: 10,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                // BlocProvider.of<CounterAppBloc>(context).add(ResetEvent());
+                //another way
+                context.read<CounterAppBloc>().add(ResetEvent());
+              },
               child: const Icon(
                 Icons.exposure_zero,
               ),
@@ -55,7 +74,9 @@ class HomeScreen extends StatelessWidget {
               height: 10,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                BlocProvider.of<CounterAppBloc>(context).add(DecrementEvent());
+              },
               child: const Icon(
                 Icons.remove,
               ),
